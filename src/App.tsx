c3d84dba9ae59c39d1dc7f651f5a401f4f5d56a4
Olly Marsters <olly.marsters@yahoo.co.uk>
2023-01-19 21:51:43 +0000

    set up env files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+const redirectUrl = import.meta.env.VITE_REDIRECT_URI;
+const authEndPoint = import.meta.env.VITE_AUTH_ENDPOINT;
+const responseType = import.meta.env.VITE_RESPONSE_TYPE;
 
 const App: Component = () => {
+  console.log(authEndPoint);
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      <a
+        href={`${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&response_type=${responseType}`}
+        target="_blank"
+      >
+        Click here
+      </a>
     </div>
   );
 };
