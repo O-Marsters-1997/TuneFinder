998cb8a7d7a430a7f79b3d03a2553670402bdc04
Olly Marsters <olly.marsters@yahoo.co.uk>
2023-01-20 17:18:57 +0000

    get the access token

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
+import { createSignal, createEffect } from "solid-js";
 import type { Component } from "solid-js";
 
 const clientId: string = import.meta.env.VITE_CLIENT_ID;
-// const clientSecret: string = import.meta.env.VITE_CLIENT_SECRET;
+const clientSecret: string = import.meta.env.VITE_CLIENT_SECRET;
 const redirectUrl: string = import.meta.env.VITE_REDIRECT_URI;
 const authEndPoint: string = import.meta.env.VITE_AUTH_ENDPOINT;
 const responseType: string = import.meta.env.VITE_RESPONSE_TYPE;
 
 const App: Component = () => {
-  console.log(authEndPoint);
+  const [token, setToken] = createSignal<string | null>("");
+
+  const getAccessToken = async () => {
+    const authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+    };
+    try {
+      const accessToken = await fetch(
+        "https://accounts.spotify.com/api/token",
+        authParameters,
+      );
+      const data = await accessToken.json();
+      return data?.access_token;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  createEffect(async () => {
+    setToken(await getAccessToken());
+  });
+
   return (
     <div>
       <a
@@ -16,6 +42,7 @@ const App: Component = () => {
       >
         Click here
       </a>
+      <p>{token()}</p>
     </div>
   );
 };
