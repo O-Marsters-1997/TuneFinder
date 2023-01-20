ef8a1e9fea9d729d5ed4ffeba44ff351fd76cadb
Olly Marsters <olly.marsters@yahoo.co.uk>
2023-01-20 18:37:52 +0000

    get allartists relavant to search query

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,11 +1,46 @@
 import type { Component } from "solid-js";
+import { createSignal } from "solid-js";
 
 interface Props {
   token?: string;
 }
 
 const Searchbar: Component<Props> = (props) => {
-  return <div>{props.token}</div>;
+  const [searchStatus, setSearchStatus] = createSignal<string>("");
+  const search = async () => {
+    // Get artist ID
+    if (searchStatus() !== "") {
+      const artistParameters = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${props.token}`,
+        },
+      };
+      const res = await fetch(
+        `https://api.spotify.com/v1/search?q=${searchStatus()}&type=artist`,
+        artistParameters,
+      );
+      const data = await res.json();
+      console.log(data.artists.items[0].name);
+    }
+  };
+
+  const handleChange = (e: any) => {
+    setSearchStatus(e.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchStatus()}
+        onInput={handleChange}
+      />
+      <button onClick={search}>Click me</button>
+    </div>
+  );
 };
 
 export default Searchbar;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import solid from "vite-plugin-solid"; // or solid-start/vite
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    devtools({
+      /* additional options */
+      autoname: true, // e.g. enable autoname
+      locator: {
+        targetIDE: "vscode",
+        componentLocation: true,
+        jsxLocation: true,
+      },
+    }),
+    solid(),
+  ],
   server: {
     port: 3000,
   },
