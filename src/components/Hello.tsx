0ed0c400239231fb897daef3bd94cba08573a7db
Olly Marsters <ollymarsters@yahoo.co.uk>
2023-01-22 12:26:25 +0000

    created context

--- src/components/Hello.tsx
+++
@@ -1,9 +0,0 @@
-import type { Component } from "solid-js";
-
-// type Props = {};
-
-const Hello: Component = () => {
-  return <div>this is a Hello</div>;
-};
-
-export default Hello;
---
+++ src/contexts/Authorisation.context.tsx
@@ -0,0 +1,7 @@
+import type { Component } from "solid-js";
+
+const Authorisation: Component = () => {
+  return <div>this is a Authorisation.context</div>;
+};
+
+export default Authorisation;
