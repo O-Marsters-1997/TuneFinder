17c9ef63edcda5bbc36691b67ce2f89d26d6c3d9
Olly Marsters <ollymarsters@yahoo.co.uk>
2023-01-24 18:44:06 +0000

    refactor on test create effect dependencies

--- src/components/ArtistItem.tsx
+++ src/components/ArtistItem.tsx
@@ -1,11 +1,31 @@
-import type { Component } from "solid-js";
+import { Component } from "solid-js";
+import { artistId, setArtistId } from "@state/artistId";
 
 type Props = {
   artist: any;
 };
 
 const ArtistItem: Component<Props> = (props) => {
-  return <div>{props.artist.name}</div>;
+  const artistIsSelected = () => {
+    return props.artist.id === artistId() ?? false;
+  };
+
+  const handleSelectArtist = () => {
+    setArtistId(props.artist.id);
+  };
+
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <p>{props.artist.name}</p>
+        {artistIsSelected() && (
+          <p style={{ color: "green" }}>This artist is selected</p>
+        )}
+        <button onClick={handleSelectArtist}>click me</button>
+      </div>
+      ,
+    </>
+  );
 };
 
 export default ArtistItem;
--- src/components/ArtistList.tsx
+++ src/components/ArtistList.tsx
@@ -5,9 +5,22 @@ import ArtistItem from "@components/ArtistItem";
 
 const ArtistList: Component = () => {
   return (
-    <For each={artists()} fallback={<div>Artists are loading</div>}>
-      {(artist) => <ArtistItem artist={artist} />}
-    </For>
+    <>
+      <For each={artists()} fallback={<div>Artists are loading</div>}>
+        {(artist) => (
+          <>
+            <ArtistItem artist={artist} />
+          </>
+        )}
+      </For>
+      <button
+        onClick={() => {
+          console.log("hello world");
+        }}
+      >
+        Click me
+      </button>
+    </>
   );
 };
 
