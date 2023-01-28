014bf21c2119c26d3a156bd2a9b6bc7b23e57320
Olly Marsters <ollymarsters@yahoo.co.uk>
2023-01-28 14:20:40 +0000

    test on prop destructuring

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -1,4 +1,5 @@
 import type { Component } from "solid-js";
+import { mergeProps } from "solid-js";
 import { Props } from "@components/lib/variants/text";
 import { textVariants } from "@myTypes/constants";
 import { getText } from "@theme/variant.helpers";
@@ -6,17 +7,20 @@ import { getTextVariant } from "@utils/style/style.helpers";
 
 const Text: Component<Props> = (_props) => {
   const props = mergeProps({ variant: textVariants.body1 }, _props);
+
+  const { text, variant, colorvariant, lineheight, sizeadjust } = props;
+
   const TextVariant = getText(props.variant);
   return (
     <TextVariant
-      text={props.text}
-      variant={props.variant}
-      as={getTextVariant(props.variant)}
-      colorvariant={props.colorvariant}
-      lineheight={props.lineheight}
-      sizeadjust={props.sizeadjust}
+      text={text}
+      variant={variant}
+      as={getTextVariant(variant)}
+      colorvariant={colorvariant}
+      lineheight={lineheight}
+      sizeadjust={sizeadjust}
     >
-      {props.text}
+      {text}
     </TextVariant>
   );
 };
