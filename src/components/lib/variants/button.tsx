7c825863bcd90709c2d32fb13bfac70bca78a90b
Olly Marsters <ollymarsters@yahoo.co.uk>
2023-01-29 17:37:50 +0000

    button component restructured

---
+++ src/components/lib/variants/button.tsx
@@ -0,0 +1,57 @@
+// import { styled } from "solid-styled-components";
+import type { Component, JSX } from "solid-js";
+import { styled } from "solid-styled-components";
+import { css } from "@emotion/css";
+import { ButtonProps as SolidButtonProps } from "@suid/material/Button";
+import { componentVariantsHoc } from "@utils/style/hoc";
+
+export type ButtonProps = {
+  text: string;
+  onClick: () => void;
+  variant?: CSS.ButtonVariants;
+  colorVariant?: string;
+  children?: JSX.Element;
+} & Omit<SolidButtonProps, "variant" | "type">;
+
+const MyButton = styled.button<ButtonProps>`
+  background-color: blue;
+  cursor: pointer;
+  color: ${({ colorVariant }) => colorVariant && colorVariant};
+`;
+
+const ButtonOtherStyles = (props: ButtonProps) => {
+  return {
+    rounded: css`
+      border-radius: 50%;
+      border: 2px solid orange;
+    `,
+  };
+};
+
+export const ButtonBase: Component<ButtonProps> = (props) => {
+  return (
+    <MyButton
+      text={props.text}
+      variant={props.variant}
+      onClick={props.onClick}
+      colorVariant={props.colorVariant}
+    >
+      {props.text}
+    </MyButton>
+  );
+};
+
+const ButtonRounded = (props: ButtonProps) => {
+  return componentVariantsHoc(
+    ButtonBase,
+    {
+      class: ButtonOtherStyles(props).rounded,
+    },
+    props,
+  );
+};
+
+export const buttonVariantsMap = {
+  base: ButtonBase,
+  rounded: ButtonRounded,
+};
